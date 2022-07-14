61b87789385afd5c029ba37adfee89f5a38cb953
Miqueias Lino <miqueias_lino001@hotmail.com>
2022-07-14 13:19:51 -0300

    Introducao Type script

---
+++ Exemplo/validacao-basica.js
@@ -0,0 +1,15 @@
+//let button = document1.getElementById("button");
+//let input1 = document1.getElementById("input1");
+//let input2 = document1.getElementById("input2");
+
+//function somarNumeros(numero1, numero2){
+  //  if(typeof numero1 === 'number' && typeof numero2 === 'number'){
+    //    return numero1 + numero2
+//} else{
+  //  return Number(numero1) + Number(numero2)
+//}
+//}
+
+//button.addEventListener('clic', () =>{
+  //  console.log(somarNumeros(input1.value, input2.value));
+//})
